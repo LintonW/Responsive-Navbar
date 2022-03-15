2118fcd80c040dedcf3698d155bf111ec4a77987
Linton <linton.warren01@gmail.com>
2022-03-15 15:41:59 +0200

    update

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-document.getElementById('open').addEventListener('click', () => {
-    document.getElementById('mobile').classList.remove('mobile-nav-links-closed');
-    document.getElementById('mobile').classList.add('mobile-nav-links-open');
-});
+const primaryNav = document.querySelector('.primary-nav');
+const navToggle = document.querySelector('.mobile-nav-toggle');
 
-document.getElementById('close').addEventListener('click', () => {
-    document.getElementById('mobile').classList.remove('mobile-nav-links-open');
-    document.getElementById('mobile').classList.add('mobile-nav-links-closed');
+navToggle.addEventListener('click', () => {
+    const visibilty = primaryNav.getAttribute('data-visible');
+
+    if (visibilty === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else if (visibilty === 'true') {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
 });
